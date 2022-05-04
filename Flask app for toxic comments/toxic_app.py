63f4ce4bd596ebaeb4e902bb9bcd118398fc1714
fyp-b14 <104861238+fyp-b14@users.noreply.github.com>
2022-05-04 13:58:06 +0530

    Added "Not Toxic" and removed print statements

--- Flask app for toxic comments/toxic_app.py
+++ Flask app for toxic comments/toxic_app.py
@@ -1,24 +1,23 @@
-
 from flask import Flask, render_template, request
 from detoxify import Detoxify
 
 app = Flask(__name__)
+
 
 # Render the HTML file for the home page
 @app.route("/")
 def home():
     return render_template('index_toxic.html')
 
+
 @app.route("/predict", methods=['POST'])
 def predict():
-    
     # Take a string input from user
     user_input = request.form['text']
     data = [user_input]
 
     results = Detoxify('original').predict(data)
 
-    print(results)
 
     pred_tox = results['toxicity']
 
@@ -33,54 +32,50 @@ def predict():
     pred_ide = results['identity_attack']
 
     out_tox = round(pred_tox[0], 2)
-    if(out_tox<0.5):
-           text_tox =""
+    if (out_tox < 0.5):
+        text_tox = "Not Toxic"
     else:
-        text_tox ="Toxic"
+        text_tox = "Toxic"
     out_sev = round(pred_sev[0], 2)
 
-    if(out_sev<0.5):
-           text_sev =""
+    if (out_sev < 0.5):
+        text_sev = ""
     else:
-        text_sev ="Severly toxic"
+        text_sev = "Severely toxic"
 
     out_obs = round(pred_obs[0], 2)
-    if(out_obs<0.5):
-           text_obs =""
+    if (out_obs < 0.5):
+        text_obs = ""
     else:
-        text_obs ="Obscene"
+        text_obs = "Obscene"
 
     out_ins = round(pred_ins[0], 2)
-    if(out_ins<0.5):
-           text_ins =""
+    if (out_ins < 0.5):
+        text_ins = ""
     else:
-        text_ins ="Insult"
+        text_ins = "Insult"
 
     out_thr = round(pred_thr[0], 2)
-    if(out_thr<0.5):
-           text_thr =""
+    if (out_thr < 0.5):
+        text_thr = ""
     else:
-        text_thr ="Threat"
+        text_thr = "Threat"
 
     out_ide = round(pred_ide[0], 2)
-    if(out_ide<0.5):
-           text_ide =""
-    elif(out_ide<0.5 and out_tox<0.5 and out_obs<0.5 and out_sev<0.5 and out_thr<0.5 and out_ins<0.5):
-        text_ide ="Good"
+    if (out_ide < 0.5):
+        text_ide = ""
     else:
-        text_ide ="Identity hate"
+        text_ide = "Identity hate"
 
-    print(out_tox)  
+    return render_template('index_toxic.html',
+                           pred_tox=text_tox,
+                           pred_sev=text_sev,
+                           pred_obs=text_obs,
+                           pred_ins=text_ins,
+                           pred_thr=text_thr,
+                           pred_ide=text_ide
+                           )
 
-    return render_template('index_toxic.html', 
-                            pred_tox =text_tox,
-                            pred_sev = text_sev,
-                            pred_obs = text_obs,
-                            pred_ins = text_ins,
-                            pred_thr = text_thr,
-                            pred_ide = text_ide                       
-                            )
 
 # Server reloads itself if code changes so no need to keep restarting:
 app.run(debug=True)
-
